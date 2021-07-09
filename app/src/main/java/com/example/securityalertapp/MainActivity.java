51a6c684095d61448c6d8fbad42e545cf711f2df
Rahulan <rahulmahendra24@gmail.com>
2021-07-09 21:49:57 +0530

    second commit

--- app/src/main/java/com/example/securityalertapp/MainActivity.java
+++ app/src/main/java/com/example/securityalertapp/MainActivity.java
@@ -1,47 +1,93 @@
 package com.example.securityalertapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+//import android.location.Address;
+//import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+//import java.util.List;
+//import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    Button button;
+    LocationManager locationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS}, PackageManager.PERMISSION_GRANTED);
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
-        String phoneNumber = "0776041139";
-        Intent intent = getIntent();
-        String latitudename = intent.getStringExtra("latitude");
-        String longitudename = intent.getStringExtra("longitude");
-        String Message = "I’m Rahulan, IM/2017/057. Please Help Me.I’m in Latitude " +latitudename+ "Longitude" + longitudename+ ".";
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
 
+        button = findViewById(R.id.button);
 
-
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(phoneNumber,null,Message,null,null);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocation();
+            }
+        });
     }
 
-    public void cordnate(View view){
-
-        Intent intent = new Intent(this, MapsActivity.class);
-            startActivity(intent);
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+        try{
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,5,MainActivity.this);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
-    public void sendAlert(){
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        Toast.makeText(this, ""+location.getLatitude()+","+location.getLongitude(),Toast.LENGTH_SHORT).show();
+
+        try{
+//            Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+//            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+//            String address = addresses.get(0).getAddressLine(0);
+
+            String phone = "0776041139";
+            double myLatitude = location.getLatitude();
+            double myLongitude = location.getLongitude();
+            String message = "My location is https://maps.google.com/?q="+myLatitude+","+myLongitude+".";
+
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phone,null,message,null,null);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
 
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
 
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
 }
